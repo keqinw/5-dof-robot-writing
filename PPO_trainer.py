75083b9ff89f316fd6f13ec2ffc047bd2aaf3ec1
keqinn <71072478+keqinn@users.noreply.github.com>
2021-12-06 15:29:56 -0500

    Add files via upload

---
+++ PPO_trainer.py
@@ -0,0 +1,47 @@
+import gym
+import gym_ur3
+import time
+from stable_baselines3 import PPO
+import imageio
+import pybullet as p
+
+def test():
+    env = gym.make('ur3-v0')
+    # obs = env.reset()
+    for _ in range(10):
+        ob = env.reset()
+        for _ in range(20):
+            p.stepSimulation()
+            time.sleep(1/24)
+        # print(1)
+        
+
+def train():
+    env = gym.make('ur3-v0')
+    model = PPO('MlpPolicy',env,verbose=1)
+    model.learn(total_timesteps=300000)
+    model.save('../UJI-3D/result/ppo-ur3-3d-1206.zip')
+
+def result():
+    frames = []
+    env = gym.make('ur3-v0')
+    model = PPO.load('../UJI-3D/result/ppo-ur3-3d-1206.zip')
+    
+    for _ in range(10):
+        obs = env.reset()
+        for i in range(50):
+            action,_state=model.predict(obs,deterministic=True)
+            obs, _, done, _ = env.step(action)
+            # print(done)
+            frames.append(env.render())
+            
+            if done:
+                # obs = env.reset()
+                time.sleep(1/30)
+                break
+    imageio.mimsave('../UJI-3D/result/ur3.gif', frames, 'GIF', duration=0.1)
+if __name__ == '__main__':
+    # test()
+    train()
+    # result()
+    
---
+++ robot_5dof.py
@@ -0,0 +1,91 @@
+import numpy as np
+import gym
+import gym_ur3
+import time
+from stable_baselines3 import PPO
+import matplotlib.pyplot as plt
+import cv2 as cv
+import imageio
+import copy 
+import pybullet as p
+
+def save2gif(frames):
+    imageio.mimsave('../UJI-3D/result/ur3_5dof.gif', frames, 'GIF', duration=0.03)
+
+def trans(ori_path,panning_x,panning_y,strech_x,strech_y):
+    ori_path = ori_path.astype(float)
+    # path = copy.deepcopy(ori_path)
+    
+    x = (ori_path[:,0]+panning_x)/strech_x
+    y = (-ori_path[:,1]+panning_y)/strech_y
+    return x,y
+
+# rectangular path 
+xs = list(np.linspace(0.1,0.2,20)) + [0.2 for i in range(20)] + list(np.linspace(0.2,0.1,20)) + [0.1 for i in range(20)]
+ys = [-0.04 for i in range(20)] + list(np.linspace(-0.04,0.04,20)) + [0.04 for i in range(20)] + list(np.linspace(0.04,-0.04,20))
+zs = [0.14 for i in range(len(xs))]
+# plt.plot(ys,zs)
+# plt.show()
+
+# circle path
+r = 0.04
+theta = np.arange(0, 2*np.pi+0.1, 0.1)
+xs = 0.15 + r * np.cos(theta)
+ys = 0 + r * np.sin(theta)
+zs = [0.14 for i in range(len(xs))]
+
+# path = np.load('../UJI-3D/result/path.npy')
+# ys,xs = trans(path[:-10],-128,-300,2560,2560)
+# xs = -xs
+# ys.reverse()
+# xs.reverse()
+plt.plot(xs,ys,'-')
+plt.title('origin path')
+plt.axis('equal')
+plt.show()
+
+
+
+# establish the environment
+env = gym.make('ur3-visual-v0')
+model = PPO.load('../UJI-3D/result/ppo-ur3-3d-1202.zip')
+
+result_x = []
+result_y = []
+frames = []
+actions = []
+
+for x,y,z in zip(xs,ys,zs):
+    obs = env.evaluate_goal((x,y,z))
+    done = False
+
+    for i in range(100):
+        if not done:
+            action,_state=model.predict(obs,deterministic=True)
+            obs, _, done, _ = env.evaluate_step(action)
+            # actions.append([-obs[-3]]+list(obs[-2:])+[obs[-1]+obs[-2]-np.pi/2])
+
+            image = env.render(224)
+            frames.append(image)
+            # frames.append(np.vstack((origin_image,env.render()[:,:,0:3])))
+        if done:
+            result_x.append(obs[3])
+            result_y.append(obs[4])
+            # let the path get more smooth
+            obs, _, done, _ = env.evaluate_step(action)
+            if i%5 == 0:
+                actions.append([-obs[-3]]+list(obs[-2:])+[obs[-1]+obs[-2]-np.pi/2])
+            break
+
+
+plt.close()
+plt.title('result path')
+plt.plot(result_x,result_y,'-',label='result path')
+plt.plot(xs,ys,'-',label='origin path')
+plt.axis('equal')
+plt.legend()
+plt.show()
+
+save2gif(frames)
+print(actions)
+np.save('../UJI-3D/result/actions.npy',actions)
